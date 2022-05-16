b3dc76e31b93ed52d1ad60bd1a2b1167f1bf0833
nur <medo0o6665@gmail.com>
2022-05-17 00:18:28 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+//start of new code
+import { useEffect, useState } from "react";
+// end of the new code
+
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+import useSpeechSynthesis from "./useSpeechSynthesis";
 
 function App() {
+  const {
+    transcript,
+    listening,
+    //start of new code
+    finalTranscript,
+    // end of the new code
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const { speak, voices } = useSpeechSynthesis();
+
+  //start of new code
+
+  const [bgColor, setBgColor] = useState("white");
+
+  const changeBGColor = (color) => {
+    speak({
+      text: `changing background to ${color}`,
+      voice: voices[7],
+    });
+  };
+
+  // end of the new code
+
+  useEffect(() => {
+    if (finalTranscript) {
+      if (finalTranscript === "can you hear me") {
+        speak({
+          text: "yes, I can , and congratulations for your first voice assistant app",
+          voice: voices[7],
+        });
+        console.log({ finalTranscript });
+      }
+      //start of new code
+      else if (
+        finalTranscript === "change the background to red"
+      ) {
+        changeBGColor("red");
+        setBgColor("red");
+      } else if (
+        finalTranscript === "change the background to White" ||
+        finalTranscript === "change the background to white"
+      ) {
+        changeBGColor("white");
+        setBgColor("white");
+      }
+      // end of the new code
+      else {
+        console.log("i don't understand");
+        speak({
+          text: "i don't understand",
+          voice: voices[7],
+        });
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [finalTranscript]);
+  if (!browserSupportsSpeechRecognition) {
+    return (
+      <span>Browser doesn't support speech recognition.</span>
+    );
+  }
+
+  const commonStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "10px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        ...commonStyle,
+        flexDirection: "column",
+        height: "100vh",
+        background: bgColor,
+      }}
+    >
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <div
+        style={{
+          ...commonStyle,
+        }}
+      >
+        <button onClick={SpeechRecognition.startListening}>
+          Start
+        </button>
+        <button onClick={SpeechRecognition.stopListening}>
+          Stop
+        </button>
+        <button onClick={resetTranscript}>Reset</button>
+      </div>
+      <p>{transcript}</p>
     </div>
   );
 }
